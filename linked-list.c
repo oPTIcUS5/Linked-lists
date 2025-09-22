0108f9a7e7e9f8f90643014c5125fa5d1f97bee4
oPTIcUS5 <ajayvega@MacBook-Air-de-Ajay.local>
2025-09-22 16:24:39 -0600

    add insert_end function

--- linked-list.c
+++ linked-list.c
@@ -1,3 +1,5 @@
+//Simple linked list
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,3 +23,38 @@ void insert_begin(node **head, int data)
         printf("no memory available\n");
     }
 }
+
+void insert_end(node **head, int data)
+{
+    node *current;
+
+    node *new_node = (node*)malloc(sizeof(node));
+
+    if(new_node != NULL)
+    {
+        new_node->data = data;
+        new_node->next_node = NULL;
+
+        if(*head == NULL)
+        {
+            *head = new_node;
+        }
+        else
+        {
+            current = *head;
+
+            while (current->next_node != NULL)
+            {
+                current = current->next_node;
+            }
+
+            current->next_node = new_node;
+        }
+    }
+    else
+    {
+        printf("no memory available\n");
+    }
+}
+
+
